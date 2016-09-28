ad10bf7c3af23bde52f01f744f77e8ed916f6e43
Alex Nisnevich <alex.nisnevich@gmail.com>
2016-09-28 12:27:07 -0700

    persistent random player color + text

---
+++ components/follow.js
@@ -0,0 +1,12 @@
+// https://rawgit.com/ngokevin/kframe/master/components/firebase/examples/basic/components/follow.js
+AFRAME.registerComponent('follow', {
+  schema: {type: 'selector'},
+
+  init: function () {
+    var el = this.el;
+    this.data.addEventListener('componentchanged', function (evt) {
+      if (evt.detail.name !== 'position') { return; }
+      el.setAttribute('position', evt.detail.newData);
+    });
+  }
+});
